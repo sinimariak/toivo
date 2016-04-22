2143e4bd75ffc782353857368b6e2d656514e814
razwanizmi <razwan.izmi@gmail.com>
2016-04-22 17:59:58 +0800

    added basic jumbotron

--- db/schema.rb
+++ db/schema.rb
@@ -47,13 +47,4 @@ ActiveRecord::Schema.define(version: 20160422063908) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "vendors", force: :cascade do |t|
-    t.string   "description"
-    t.string   "video"
-    t.string   "category"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
 end
