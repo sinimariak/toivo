85cdc4025953fe1ff4b5df11d8fa7182e03cef95
Aaron <Ang>
2016-04-27 10:07:57 +0800

    search feature works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
   def show
   end
-
-  def search
-  	User.reindex
-	user = User.search 
-	user.each do |user|
-	  puts user.details
-    end
-  end
 end
