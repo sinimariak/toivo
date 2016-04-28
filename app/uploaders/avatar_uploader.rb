e8edeb00f3b36bfd243627ced0bc959fa3d3fd17
razwanizmi <razwan.izmi@gmail.com>
2016-04-28 09:17:11 +0800

    updated user profile page

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,7 +33,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fill => [50, 50]
+  end
+
+  version :profile do
+    process :resize_to_fill => [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
